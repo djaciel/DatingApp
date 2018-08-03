34350cb15fb6fd45f5d00b9ddae3b358b22456c9
Dario J. Gonzalez Orero <gonzalezotero.5@db.merkafon.com>
2018-08-03 17:08:32 -0500

    End of section 3

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must spicify pwd between 4 and 8 char")]
+        public string Password { get; set; }
+    }
+}
